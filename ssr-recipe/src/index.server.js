87c5584c16b712bdb8eaf8451026148cf695a8bb
Rilyaus <rilyaus.gd@gmail.com>
2021-08-16 17:08:14 +0900

    feat : default build - SSR

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -3,6 +3,42 @@ import ReactDOMServer from "react-dom/server";
 import express from "express";
 import { StaticRouter } from "react-router-dom";
 import App from "./App";
+import path from "path";
+import fs from "fs";
+
+const manifest = JSON.parse(
+  fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8")
+);
+
+const chunks = Object.keys(manifest.files)
+  .filter(key => /chunk\.js$/.exec(key))
+  .map(key => `<script src="${manifest.files[key]}"></script>`)
+  .join("");
+
+function createPage(root) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
+      <meta name="theme-color" content="#000000" />
+      <title>React App</title>
+      <link href="${manifest.files["main.css"]}" rel="stylesheet" />
+    </hed>
+    <body>
+      <noscript>You need to enable Js to run this app.</noscript>
+      <div id="root">
+        ${root}
+      </div>
+      <script src="${manifest.files["runtime-main.js"]}"></script>
+      ${chunks}
+      <sciprt src="${manifest.files["main.js"]}"></sciprt>
+    </body>
+  </html>`;
+}
 
 const app = express();
 
@@ -15,8 +51,15 @@ const serverRender = (req, res, next) => {
   );
   const root = ReactDOMServer.renderToString(jsx);
 
-  res.send(root);
+  res.send(createPage(root));
 };
+
+const serve = express.static(path.resolve("./build"), {
+  index: false,
+});
+
+// befor ServerRender
+app.use(serve);
 
 app.use(serverRender);
 
