2cfe046c8746a2ce1387e5a65f5385ec8fc8c1e3
Rilyaus <rilyaus.gd@gmail.com>
2021-08-22 12:26:29 +0900

    feat : [blog] - JWT 회원 인증 구현(M1 bcrypt 이슈)

---
+++ blog/blog-backend/src/models/user.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// Methods
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+
+  return data;
+};
+
+// Statics
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
