20ca0ef740b582e255c44f90acbf622e0bcf02e6
Rilyaus <rilyaus.gd@gmail.com>
2021-08-24 07:18:43 +0900

    feat : [blog-FE] Base Page and Component

---
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * 회원가입 또는 로그인 폼
+ */
+
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+  return (
+    <AuthFormBlock>
+      <p>AuthForm</p>
+    </AuthFormBlock>
+  );
+};
+
+export default AuthForm;
