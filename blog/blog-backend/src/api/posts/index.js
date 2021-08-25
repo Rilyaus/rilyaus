45c2896a55958943f977c8bab8592e55433ae854
Rilyaus <rilyaus.gd@gmail.com>
2021-08-25 16:59:14 +0900

    feat : [blog-BE] 로그인 체크 기능 미들웨어 추가

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,16 +1,17 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
 
 post.get('/:id', postsCtrl.read);
-post.delete('/:id', postsCtrl.remove);
-post.patch('/:id', postsCtrl.update);
+post.delete('/:id', checkLoggedIn, postsCtrl.remove);
+post.patch('/:id', checkLoggedIn, postsCtrl.update);
 
 // ObjectId 검증
 posts.use('/:id', postsCtrl.checkObjectId, post.routes());
